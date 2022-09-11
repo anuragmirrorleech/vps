49d198df874ddd6896819c7a53b2eaf619ad1832
haashim007 <102474562+haashim007@users.noreply.github.com>
2022-09-11 21:10:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -387,14 +387,15 @@ async def get_shortlink(link):
               'url': link,
               }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            if data["status"] == "success":
-                return data['shortenedUrl']
-            else:
-                 logger.error(f"Error: {data['message']}")
-                 return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json(content_type='text/html')
+                if data["status"] == "success":
+                    return data['shortlink']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
