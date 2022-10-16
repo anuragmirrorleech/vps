0f0c6b0011b636c70cfc3e8d32bf6d5894db2006
Greymattersbot <110471652+Greymattersbot@users.noreply.github.com>
2022-10-17 01:06:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -75,7 +75,7 @@ async def next_page(bot, query):
     btn.insert(0, 
         [
             InlineKeyboardButton('ɢʀᴏᴜᴘ', url='search_zone_support'),
-            InlineKeyboardButton('sʜᴀʀᴇ ᴍᴇ', url="https://t.me/share/url?url=%2A%2A%F0%9F%98%B1+Search+Zone%C2%B2%C2%B7%E2%81%B0+%F0%9F%98%B1+%0D%0A+%0D%0AWhatever+you+might+ask%2C+you+will+get..+%E2%9D%A4%EF%B8%8F+%0D%0A+%0D%0A%F0%9F%91%87+Join%0D%0A+https%3A%2F%2Ft.me%2FSearch_Zone"),
+            InlineKeyboardButton('sʜᴀʀᴇ ᴍᴇ', url='https://t.me/share/url?url=%2A%2A%F0%9F%98%B1+Search+Zone%C2%B2%C2%B7%E2%81%B0+%F0%9F%98%B1+%0D%0A+%0D%0AWhatever+you+might+ask%2C+you+will+get..+%E2%9D%A4%EF%B8%8F+%0D%0A+%0D%0A%F0%9F%91%87+Join%0D%0A+https%3A%2F%2Ft.me%2FSearch_Zone'),
             InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/search_zone')
         ]
     )
