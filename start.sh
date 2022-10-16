0e7b8267808a7bb0f05b05bf30c1fb8cec2e7098
Greymattersbot <110471652+Greymattersbot@users.noreply.github.com>
2022-10-16 19:37:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/haashim007/EvaMaria.git /EvaMaria
+  git clone https://github.com/Greymattersbot/vps /vps
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /vps
 fi
-cd /EvaMaria
+cd /vps
 pip3 install -U -r requirements.txt
 echo "Starting Bot url...."
 python3 bot.py
