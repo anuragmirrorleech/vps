dfb27ba9c213c614f4f0a71582272b3214042dcd
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-10-17 23:50:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/vps /vps
+  git clone https://github.com/anuragmirrorleech/vps /vps
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /vps
